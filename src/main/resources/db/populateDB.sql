22545d0e0ee8439a514a6a44ecce41195d8c3457
qssha <qssha@yandex.ru>
2020-06-23 08:15:42 +0300

    First part

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -9,3 +10,12 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, datetime, description, calories)
+VALUES (100000, '2020-01-30 10:00', 'Завтрак', 500),
+       (100000, '2020-01-30 13:00', 'Обед', 1000),
+       (100000, '2020-01-30 20:00', 'Ужин', 500),
+       (100001, '2020-01-31 00:00', 'Еда на граничное значение', 100),
+       (100001, '2020-01-31 10:00', 'Завтрак', 1000),
+       (100001, '2020-01-31 13:00', 'Обед', 500),
+       (100001, '2020-01-31 20:00', 'Ужин', 410);
