1ab62160a44e712ecadf97db44a33a4c96065f91
qssha <qssha@yandex.ru>
2020-07-29 20:07:15 +0300

    LocalDateConverter/LocalTimeConverter try/catch removed

--- src/main/java/ru/javawebinar/topjava/converter/LocalDateConverter.java
+++ src/main/java/ru/javawebinar/topjava/converter/LocalDateConverter.java
@@ -5,16 +5,11 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 @Component
 public class LocalDateConverter implements Converter<String, LocalDate> {
     @Override
     public LocalDate convert(String source) {
-        try {
-            return LocalDate.parse(source, DateTimeFormatter.ISO_LOCAL_DATE);
-        } catch (DateTimeParseException e) {
-            return null;
-        }
+        return LocalDate.parse(source, DateTimeFormatter.ISO_LOCAL_DATE);
     }
 }
--- src/main/java/ru/javawebinar/topjava/converter/LocalTimeConverter.java
+++ src/main/java/ru/javawebinar/topjava/converter/LocalTimeConverter.java
@@ -5,16 +5,11 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 @Component
 public class LocalTimeConverter implements Converter<String, LocalTime> {
     @Override
     public LocalTime convert(String source) {
-        try {
-            return LocalTime.parse(source, DateTimeFormatter.ISO_LOCAL_TIME);
-        } catch (DateTimeParseException e) {
-            return null;
-        }
+        return LocalTime.parse(source, DateTimeFormatter.ISO_LOCAL_TIME);
     }
 }
