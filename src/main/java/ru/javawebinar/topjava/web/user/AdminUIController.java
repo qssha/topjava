ff5986f022b03fa77e1a46114764ec5ce5fe9a02
qssha <qssha@yandex.ru>
2020-08-12 22:43:23 +0300

    Imports optimized

--- src/main/java/ru/javawebinar/topjava/web/user/AdminUIController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminUIController.java
@@ -11,7 +11,6 @@ import ru.javawebinar.topjava.util.BindingUtil;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/admin/users")
