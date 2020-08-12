6dd9adc59785051d4661824d9811fafad5fc1634
qssha <qssha@yandex.ru>
2020-08-12 14:17:55 +0300

    Spring Validation

---
+++ src/main/java/ru/javawebinar/topjava/util/BindingUtil.java
@@ -0,0 +1,17 @@
+package ru.javawebinar.topjava.util;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+
+import java.util.stream.Collectors;
+
+public class BindingUtil {
+    private BindingUtil() {}
+
+    public static ResponseEntity<String> processBindingResult(BindingResult result) {
+        String errorFieldsMsg = result.getFieldErrors().stream()
+                .map(fe -> String.format("[%s] %s", fe.getField(), fe.getDefaultMessage()))
+                .collect(Collectors.joining("<br>"));
+        return ResponseEntity.unprocessableEntity().body(errorFieldsMsg);
+    }
+}
