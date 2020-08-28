6eae3c9e34020ca8bc52ff0215f072298383c19f
qssha <qssha@yandex.ru>
2020-08-28 03:23:41 +0300

    hr.sh added to repo

---
+++ hr.sh
@@ -0,0 +1,2 @@
+mvn -B -s settings.xml -DskipTests=true clean package
+java -Dspring.profiles.active="datajpa,heroku" -DDATABASE_URL="postgres://user:password@localhost:5432/topjava" -jar target/dependency/webapp-runner.jar target/*.war
